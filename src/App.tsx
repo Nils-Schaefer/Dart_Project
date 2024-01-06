cb0335b13e046744db6c4830973afd8f65c69c21
Nils-Schaefer <Nils1401@gmail.com>
2024-01-06 20:23:09 +0100

    styling adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css';
+import styles from './app.module.css';
 import { Usercard } from './components/usercard';
 import { v4 as uuid } from 'uuid';
 type User = { name: string; score: number; id: string };
@@ -8,16 +8,20 @@ function App() {
   const [users, setUsers] = useState<User[]>([
     { name: 'anna', score: 301, id: uuid() },
     { name: 'bernd', score: 201, id: uuid() },
+    { name: 'carsten', score: 501, id: uuid() },
+    { name: 'doris', score: 401, id: uuid() },
   ]);
   return (
-    <div>
-      {users.map((user) => (
-        <Usercard
-          key={user.id}
-          name={user.name}
-          score={user.score}
-        ></Usercard>
-      ))}
+    <div className={styles.appwraper}>
+      <div className={styles.app}>
+        {users.map((user) => (
+          <Usercard
+            key={user.id}
+            name={user.name}
+            score={user.score}
+          ></Usercard>
+        ))}
+      </div>
     </div>
   );
 }
